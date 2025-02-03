ea7f88957ce2bdd7c53598a4c8e363d6d238b4cb
Phani <rajkumar.bommaraju@infomagnus.com>
2025-02-03 22:19:50 +0530

    Add migration scripts and update README for Gitea to GitHub migration

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
 # Kintsugimigration-scripts
+
+## Overview
+This repository contains scripts to migrate repositories from Gitea to GitHub.
+
+## Scripts
+
+### migrate-multipe-repo.sh
+This script migrates multiple repositories from Gitea to GitHub using a CSV file as input.
+
+#### Usage
+```sh
+./migrate-multipe-repo.sh <GITEA_USERNAME> <GITEA_PASSWORD> <GITHUB_ORG> <GITHUB_TOKEN> <CSV_FILE>
+```
+example: ./migrate-multipe-repo.sh giteaUser giteaPass githubOrg githubToken migration.csv
+
+#### CSV File
+The migration.csv file contains the list of repositories to be migrated. Each line should contain the Gitea organization and repository name separated by a comma.
+
+Example:
+
+gitea-org,gitea-repo1
+
+gitea-org,gitea-repo2
+
+### migrate-single-repo.sh
+This script migrates a single repository from Gitea to GitHub.
+
+#### Usage
+```sh
+./migrate-single-repo.sh <GITEA_USERNAME> <GITEA_PASSWORD> <GITEA_ORG> <GITEA_REPO> <GITHUB_ORG> <GITHUB_TOKEN>
+```
+example: ./migrate-single-repo.sh giteaUser giteaPass giteaOrg giteaRepo githubOrg githubToken
+
---
+++ migrate-single-repo.sh
@@ -0,0 +1,45 @@
+# migrate repos from gitea to Github org
+
+# Inputs
+GITEA_USERNAME=$1
+GITEA_PASSWORD=$2
+GITEA_ORG=$3
+GITEA_REPO=$4
+GITHUB_ORG=$5
+GITHUB_TOKEN=$6
+
+# Remove any carriage return characters
+GITEA_REPO=$(echo $GITEA_REPO | tr -d '\r')
+
+# Mirror clone the Gitea repository
+GIT_SSL_NO_VERIFY=true git clone --mirror https://${GITEA_USERNAME}:${GITEA_PASSWORD}@gitea.com/${GITEA_ORG}/${GITEA_REPO}.git
+
+# Change to the repository directory
+cd ${GITEA_REPO}.git
+
+# Create a new repository on GitHub using the GitHub API
+echo "creating repo on github"
+response=$(curl -s -o /dev/null -w "%{http_code}" -L -X POST -H "Accept: application/vnd.github+json" -H "Authorization: Bearer ${GITHUB_TOKEN}" -H "X-GitHub-Api-Version: 2022-11-28" https://sandbox.kintsugi.ai/api/v3/orgs/${GITHUB_ORG}/repos -d "{\"name\":\"${GITEA_REPO}\",\"visibility\":\"internal\"}")
+
+if [ "$response" -eq 201 ]; then
+echo "Repository ${GITEA_REPO} created successfully on GitHub."
+else
+echo "Failed to create repository ${GITEA_REPO} on GitHub. Status code: $response"
+fi
+
+# Set the new remote URL for GitHub
+echo "setting remote url"
+echo "url: https://sandbox.kintsugi.ai/${GITHUB_ORG}/${GITEA_REPO}.git"
+git remote set-url origin https://sandbox.kintsugi.ai/${GITHUB_ORG}/${GITEA_REPO}.git
+
+# Remove pull request refs
+git for-each-ref --format='%(refname)' refs/pull | xargs -n 1 git update-ref -d
+
+# Mirror push to GitHub
+echo "pushing to github"
+GIT_SSL_NO_VERIFY=true git push --mirror
+
+# Go back to the parent directory
+cd ..
+
+rm -rf ${GITEA_REPO}.git
